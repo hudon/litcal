eb7c4be7d29fefccc50d6a0a4ce01d1001e44b26
James Hudon <warthog_myriads.0t@icloud.com>
2024-01-30 13:58:29 -0800

    updated event_key for all lit_celebration in litcal.sqlite

--- etl/load/load.go
+++ etl/load/load.go
@@ -1,4 +1,4 @@
-package load
+package main
 
 import (
 	"database/sql"
@@ -14,6 +14,32 @@ type EventJSON struct {
 }
 type LitcalJSON struct {
 	Events map[string]EventJSON `json:"events"`
+}
+
+// TODO: backup the db before loading
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run load.go <db_file> <json_file>")
+		return
+	}
+
+	dbFile := os.Args[1]
+	jsonFile := os.Args[2]
+
+	db, err := sql.Open("sqlite3", dbFile)
+	if err != nil {
+		fmt.Printf("Failed to open database: %v\n", err)
+		return
+	}
+	defer db.Close()
+
+	err = AddUniqueEventKeysFromFile(db, jsonFile)
+	if err != nil {
+		fmt.Printf("Failed to add unique event keys: %v\n", err)
+		return
+	}
+
+	fmt.Println("Unique event keys added successfully!")
 }
 
 func LoadData(db *sql.DB, filename string) error {
@@ -33,7 +59,6 @@ func AddUniqueEventKeysFromFile(db *sql.DB, jsonFilename string) error {
 		return fmt.Errorf("failed to parse JSON data: %w", err)
 	}
 
-	// Call the AddUniqueEventKeysFromJSON function with the parsed JSON
 	err = addUniqueEventKeys(db, parsedData)
 	if err != nil {
 		return err
@@ -43,7 +68,19 @@ func AddUniqueEventKeysFromFile(db *sql.DB, jsonFilename string) error {
 }
 
 func addUniqueEventKeys(db *sql.DB, data LitcalJSON) error {
-	// TODO: implement;
-	// iterate over the events, use the date as the key to look for the celebration in the sqlite db
-	// then take the key of the vent and insert it as event_key into the table
+	for key, event := range data.Events {
+		// Use the date to find the lit_celebration by joining with lit_day
+		// then update the lit_celebration with the event_key
+		query := `
+			UPDATE lit_celebration
+			SET event_key = ?
+			WHERE lit_day_id IN (select id from lit_day where lit_day.secular_date_s = ?)
+			`
+		_, err := db.Exec(query, key, event.Date)
+		if err != nil {
+			return fmt.Errorf("failed to update lit_celebration: %w", err)
+		}
+	}
+
+	return nil
 }
