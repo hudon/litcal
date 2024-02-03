4244074ce21efcc663c15ed12942978d113de30b
James Hudon <warthog_myriads.0t@icloud.com>
2024-02-03 15:48:37 -0800

    make the dates view cell

---
+++ litcal-ios/litcal-ios/CircularView.h
@@ -0,0 +1,12 @@
+//
+//  CircularView.h
+//  litcal-ios
+//
+//  Created by James Hudon on 2/3/24.
+//
+
+#ifndef CircularView_h
+#define CircularView_h
+
+
+#endif /* CircularView_h */
---
+++ litcal-ios/litcal-ios/CircularView.m
@@ -0,0 +1,8 @@
+//
+//  CircularView.m
+//  litcal-ios
+//
+//  Created by James Hudon on 2/3/24.
+//
+
+#import <Foundation/Foundation.h>
