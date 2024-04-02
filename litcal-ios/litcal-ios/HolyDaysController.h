113e88f78cd22d2389184114336539049c96a0bf
James Hudon <warthog_myriads.0t@icloud.com>
2024-04-02 09:48:08 -0700

    start holy days modal

---
+++ litcal-ios/litcal-ios/HolyDaysController.h
@@ -0,0 +1,16 @@
+//
+//  HolyDaysController.h
+//  litcal-ios
+//
+//  Created by James Hudon on 4/2/24.
+//
+
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface HolyDaysController : UIViewController
+
+@end
+
---
+++ litcal-ios/litcal-ios/HolyDaysController.m
@@ -0,0 +1,8 @@
+//
+//  HolyDaysController.m
+//  litcal-ios
+//
+//  Created by James Hudon on 4/2/24.
+//
+
+#import "HolyDaysController.h"
