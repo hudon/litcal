e6976e5a6e2a6929ff79c4bf3537f3c18129a064
James Hudon <warthog_myriads.0t@icloud.com>
2024-04-11 21:22:32 -0700

    init api

---
+++ web/litcal-web-api/src/main/java/com/litcal/web/api/LitcalWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.litcal.web.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitcalWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LitcalWebApiApplication.class, args);
+    }
+
+}
