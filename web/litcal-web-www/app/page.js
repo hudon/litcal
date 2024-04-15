7291393b3113ba8fd7284efb4401b29a7afc27a2
James Hudon <warthog_myriads.0t@icloud.com>
2024-04-14 20:43:46 -0700

    more layout work; TODO: bar color

--- web/litcal-web-www/app/page.js
+++ web/litcal-web-www/app/page.js
@@ -1,26 +1,6 @@
-import Image from "next/image";
+import {Fragment} from 'react'
+import {CalendarIcon,} from '@heroicons/react/24/outline'
 
-import { Fragment } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import {
-	Bars3Icon,
-	CalendarIcon,
-	ChartPieIcon,
-	DocumentDuplicateIcon,
-	FolderIcon,
-	HomeIcon,
-	UsersIcon,
-	XMarkIcon,
-} from '@heroicons/react/24/outline'
-
-const navigation = [
-	{ name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-	{ name: 'Team', href: '#', icon: UsersIcon, current: false },
-	{ name: 'Projects', href: '#', icon: FolderIcon, current: false },
-	{ name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-	{ name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-	{ name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
-]
 const teams = [
 	{ id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
 	{ id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
@@ -46,11 +26,10 @@ function NavHolyDays() {
 			'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
 		)}
 	>
-		<span
-			className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white"
-		>
-			{team.initial}
-		</span>
+		<CalendarIcon
+			className='text-white h-6 w-6 shrink-0'
+			aria-hidden="true"
+		/>
 		<span className="truncate">Holy Days of obligation</span>
 	</a>
 }
@@ -60,13 +39,8 @@ function NavFooter() {
 		href="#"
 		className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-indigo-700"
 	>
-		<img
-			className="h-8 w-8 rounded-full bg-indigo-700"
-			src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-			alt=""
-		/>
-		<span className="sr-only">Your profile</span>
-		<span aria-hidden="true">Tom Cook</span>
+		<span className="sr-only">Download on the App Store</span>
+		<span aria-hidden="true">Download on the App Store</span>
 	</a>
 }
 
