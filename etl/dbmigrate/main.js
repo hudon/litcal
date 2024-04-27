5a0782b4f2758a8818f0b6db252dfc8f6b79e25e
James Hudon <warthog_myriads.0t@icloud.com>
2024-04-26 21:07:29 -0700

    wip

--- etl/dbmigrate/main.js
+++ etl/dbmigrate/main.js
@@ -1,14 +1,64 @@
-import readline from "node:readline"
+import process from "node:process"
+import Database from "better-sqlite3"
+import backup from "./backup.js"
+import migrate from "./migrate.js"
 
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-})
+function fatal(s) {
+	console.error(s)
+	process.exit(1)
+}
 
-rl.question(`What's your name?`, name => {
-	console.log(`Hi ${name}!`);
-	rl.close();
+/**
+ *
+ * @param {Array<string>} a
+ * @return {[Database, string, number, number]}
+ */
+function parseArgs(a) {
+	let argv = a.slice(1)
+	if (argv.length < 2) {
+		fatal("SQLite file path argument is required")
+	}
 
+	const sqlitePath = process.argv[1]
 
+	const db = new Database(sqlitePath, {
+		fileMustExist: true,
+	})
 
-});
+	process.on("exit", () => db.close())
+	process.on("SIGHUP", () => process.exit(128 + 1))
+	process.on("SIGINT", () => process.exit(128 + 2))
+	process.on("SIGTERM", () => process.exit(128 + 15))
+
+	let targetVersion = null,
+		secondTarget = null,
+		migrationsPath = process.cwd()
+
+	if (argv.length > 2) {
+		migrationsPath = process.argv[2]
+	}
+	if (argv.length > 3) {
+		targetVersion = parseInt(process.argv[2])
+		if (targetVersion < 0) {
+			fatal("Invalid target version")
+		}
+	}
+	if (argv.length > 4) {
+		secondTarget = parseInt(process.argv[3])
+		if (secondTarget < 0) {
+			fatal("Invalid second target version")
+		}
+	}
+	return [db, migrationsPath, targetVersion, secondTarget]
+}
+
+export default function main() {
+	try {
+		let [db, migPath, t1, t2] = parseArgs(process.argv)
+		backup(migPath)
+		migrate(db, migPath, t1)
+		if (t2 !== null) migrate(db, migPath, t2)
+	} catch (error) {
+		fatal(error.message)
+	}
+}
